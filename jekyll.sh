707013ba457d80db5c6d2d1eba58761f0b602127
Phill Conrad <pconrad@cs.ucsb.edu>
2019-01-01 10:15:53 -0800

    update to new theme

--- jekyll.sh
+++ jekyll.sh
@@ -2,5 +2,5 @@
 
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-rvm use ruby-2.5.1
-bundle exec jekyll serve
+rvm use 2.5.1
+bundle exec jekyll serve $@
--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ echo "Installing software needed to run Jekyll locally... "
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
 rvm install ruby-2.5.1
-rvm use ruby-2.5.1
-gem install bundler
+rvm use 2.5.1
+gem install bundler 
+gem install pkg-config
+gem install nokogiri
+# bundle config build.nokogiri --use-system-libraries
 bundle install --path vendor/bundle
 echo "Done."
