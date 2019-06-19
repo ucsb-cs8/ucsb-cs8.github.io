fe198b803d0d263c57b9e814649b9efbfd764e4d
Phill Conrad <pconrad@cs.ucsb.edu>
2019-06-19 08:46:20 -0700

    update navigation

--- jekyll.sh
+++ jekyll.sh
@@ -2,5 +2,6 @@
 
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-rvm use 2.5.1
+rvm use 2.5.1 --install --binary --fuzzy
+bundle install --jobs=3 --retry=3
 bundle exec jekyll serve $@
