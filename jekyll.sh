4952573b21de59eba8bdc702da43ab0d8debb790
Phill Conrad <pconrad@cs.ucsb.edu>
2019-09-24 22:52:32 -0700

    pc - update for Ruby 2.5.3

--- jekyll.sh
+++ jekyll.sh
@@ -2,6 +2,5 @@
 
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-rvm use 2.5.1 --install --binary --fuzzy
-bundle install --jobs=3 --retry=3
+rvm use 2.5.3
 bundle exec jekyll serve $@
