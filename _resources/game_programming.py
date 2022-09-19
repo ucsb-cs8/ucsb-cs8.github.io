17111275d996c101481f1610144bf2c1091e31d4
Phill Conrad <pconrad@cs.ucsb.edu>
2022-09-19 11:47:31 -0700

    Create game_programming.py

---
+++ _resources/game_programming.py
@@ -0,0 +1,11 @@
+---
+topic: "Game Programming"
+desc: "Game Programming in Python"
+---
+
+* <https://www.pygame.org/wiki/about>
+  - "Pygame is a set of Python modules designed for writing video games. Pygame adds functionality on top of the excellent SDL library. This allows you to create fully featured games and multimedia programs in the python language."
+* <https://grantjenks.com/docs/freegames/>: 
+  - "Free Python Games is an Apache2 licensed collection of free Python games intended for education and fun. The games are written in simple Python code and designed for experimentation and changes. Simplified versions of several classic arcade games are included."
+  
+  
